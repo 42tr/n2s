1d8adcb30d56b70065d7883fe139859b6c68baa1
42tr <1055894396@qq.com>
2025-08-18 09:28:25 +0800

    修改 fmt 风格

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,14 @@ use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let cors = CorsLayer::new().allow_origin(Any).allow_methods(Any).allow_headers(Any).allow_credentials(false);
-    let app = axum::Router::new().route("/health", axum::routing::get(|| async { "OK" })).layer(cors);
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any)
+        .allow_credentials(false);
+    let app = axum::Router::new()
+        .route("/health", axum::routing::get(|| async { "OK" }))
+        .layer(cors);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
     Ok(())
