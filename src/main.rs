90f89b5f63588efaf0b805daef99f4733c63e19d
42tr <1055894396@qq.com>
2025-08-18 09:26:39 +0800

    使用 axum 作为后端

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use tower_http::cors::{Any, CorsLayer};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let cors = CorsLayer::new().allow_origin(Any).allow_methods(Any).allow_headers(Any).allow_credentials(false);
+    let app = axum::Router::new().route("/health", axum::routing::get(|| async { "OK" })).layer(cors);
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+    Ok(())
 }
